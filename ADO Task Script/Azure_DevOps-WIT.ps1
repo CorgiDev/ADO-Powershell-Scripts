fe58b249b38d05ad2863ee0ce1030d3b3f9e5d14
Erissa Renée Duvall <corgidev@outlook.com>
2022-09-18 03:27:04 -0400

    Initial file upload.

---
+++ ADO Task Script/Azure_DevOps-WIT.ps1
@@ -0,0 +1,63 @@
+﻿############################################
+# Make sure Azure CLI and Azure DevOps
+# extension updated
+############################################
+# az upgrade
+
+############################################
+# Variables
+############################################
+$azPAT = "PLACEHOLDER FOR ACTUAL TOKEN"
+$organizationName = "ORG NAME PLACEHOLDER"
+$projectName = "PROJECT NAME PLACEHOLDER"
+$orgURL = "https://dev.azure.com/$organizationName/"
+$sprint = "Sprint 20"
+
+############################################
+# Connect to Azure DevOps Instance with PAT
+############################################
+Write-Output $azPAT | devops login --organization $orgURL
+#az login
+
+############################################
+# Work Item Array
+############################################
+$workItems = @(
+   [pscustomobject]@{Type="Issue";Title="Test Issue 1";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 2";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 3";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 4";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 5";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 6";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 7";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 8";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+   [pscustomobject]@{Type="Issue";Title="Test Issue 9";Description="<div>Test Issue description.</div>";Activity="";Area=$projectName;Iteration="$projectName\$sprint";AssignedTo="[Name Placeholder]";Parent="[Parent Number Placeholder]"}
+)
+
+############################################
+# Begin Work Item Creation and Relate to
+# Parent Work Item
+############################################
+# TODO: Iterate through array of work items
+$workItems | ForEach-Object {
+    # Temporary variables
+    $type =$_.Type 
+    $title=$_.Title
+    $description = $_.Description
+    $activity=$_.Activity
+    $area=$_.Area
+    $iteration=$_.Iteration
+    $assignedTo=$_.AssignedTo
+    $parent=$_.Parent
+
+    # TODO: Create the work item
+    $resultJson = az boards work-item create --project $projectName --title $title --type $type --description $description --organization $orgURL --fields "Microsoft.VSTS.Common.Activity=$activity" "System.AreaPath=$area" "System.IterationPath=$iteration" "System.AssignedTo=$assignedTo"
+    $callResult = $resultJson | ConvertFrom-Json
+    $newWitID = $callResult.id
+
+    # TODO: Assign it to its parent epic
+    az boards work-item relation add --id $newWitID --relation-type parent --target-id $parent
+
+   $resultJson | Out-File -FilePath %USERPROFILE%\Desktop\resultJson.txt
+   $callResult | Out-File -FilePath %USERPROFILE%\Desktop\callResult.txt
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ADO_Powershell_Fun
-Powershell scripts for doing stuff in ADO
+Powershell scripts for doing stuff in ADO.
+
+## Azure_DevOps-WIT.ps1
+
+- **Purpose:** This script for creating some tasks in Azure DevOps.
+
+### Pre-requisites
+
+You must do the following before you can successfully run this script:
+1. Install Azure CLI from:
+    1. https://learn.microsoft.com/en-us/cli/azure/install-azure-cli
+2. Install the Azure DevOps Powershell Extension using the following command in Powershell:
+    1. `az extension add --upgrade -n azure-devops`
+3. Get a PAT from Azure DevOps that has the ability to read/write/manage work items in the project you wish to manage.
